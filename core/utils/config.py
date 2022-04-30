a26741a954049ff1fec725dc9b4c1296cdc44f7a
fecodoo <fecodoo@gmail.com>
2022-04-30 22:29:24 +0300

    remove scripts under root directory
    Former-commit-id: 8a04a65b052c624e41b82412b6abcc211f3d65c6

--- core/utils/config.py
+++ core/utils/config.py
@@ -1,22 +1,19 @@
-from core.utils.types import PathLike
 from dataclasses import dataclass
 
 
-@dataclass
+@dataclass(repr=True)
 class LipNetConfig:
-    model_weight_path: PathLike
-    video_path: PathLike
-    frame_count: int = (75,)
-    image_channels: int = (3,)
-    image_height: int = (50,)
-    image_width: int = (100,)
-    max_string: int = (32,)
-    output_size: int = (28,)
+    frame_count: int = 75
+    image_channels: int = 3
+    image_height: int = 50
+    image_width: int = 100
+    max_string: int = 32
+    output_size: int = 28
 
-@dataclass
-class BaselineConifg:
-    model_weight_path: PathLike
-    image_path: PathLike
-    image_channels: int = (3,)
-    image_height: int = (244,)
-    image_width: int = (244,)
+
+@dataclass(repr=True)
+class DenseNetConfig:
+    batch_size: int = 75
+    image_channels: int = 3
+    image_height: int = 244
+    image_width: int = 244
