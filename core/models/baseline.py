c40510ec6b78257802b0d66f6ce1f2d01866e32f
fecodoo <fecodoo@gmail.com>
2022-05-09 22:39:54 +0300

    video random sampler
    Former-commit-id: d755cd725f11223a49c7b1974fe1b9d946fc95c6

--- core/models/baseline.py
+++ core/models/baseline.py
@@ -1,18 +1,27 @@
 import os
+from dataclasses import dataclass
 from numpy import ndarray
+from tensorflow.keras import Sequential
 from tensorflow.keras.applications.densenet import DenseNet121, preprocess_input
 from tensorflow.keras.losses import categorical_crossentropy
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import Model
 from tensorflow.keras.backend import function, image_data_format
-from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization, Input
+from tensorflow.keras.layers import RandomFlip, RandomRotation, RandomContrast, Rescaling
 from core.utils.types import Stream
 
-# params
-ADAM_LEARN_RATE = 1e-3
-
 class DenseNet(object):
-    def __init__(self, config) -> None:
+    def __init__(self, config: dataclass, mode="training") -> None:
+        """init baseline model
+
+        Args:
+            config (dataclass): pre-trained model config
+            mode (str, optional): choose structure for different purpose. Defaults to "training".
+
+        Raises:
+            ValueError: if mode is neither training nor predicting
+        """
         input_shape = self.get_input_shape(          
             config.image_height,
             config.image_width,
@@ -26,50 +35,64 @@ class DenseNet(object):
             pooling="avg",
         )
 
-        self.densenet_custom_dense_0 = Dense(
-            name="densenet_custom_dense_0",
-            units=256,
+        # Freeze the base_model
+        self.base_model.trainable = False
+
+        self.densenet_inputs = Input(shape=input_shape, name="densenet_custom_input_layer")
+        
+        if mode == "training":
+            self.densenet_argumentation = Sequential(
+                layers=[RandomFlip("horizontal"), RandomRotation(0.1), RandomContrast(0.1)],
+                name="densenet_custom_argumentation_layer"
+            )(self.densenet_inputs)
+
+            self.densenet_rescaling_layer = Rescaling(
+                scale=1 / 127.5,
+                offset=-1,
+                name="densenet_custom_rescaling_layer"
+            )(self.densenet_argumentation)
+
+        elif mode == "predicting":
+            self.densenet_rescaling_layer = Rescaling(
+                scale=1 / 127.5,
+                offset=-1,
+                name="densenet_custom_rescaling_layer"
+            )(self.densenet_inputs)
+        else:
+            raise ValueError("mode can only be either training or predicting")
+            
+        self.densenet_basemodel_output = self.base_model(self.densenet_rescaling_layer, training=False)
+
+        self.densenet_custom_layer_0 = Dense(
+            name="densenet_custom_layer_0",
+            units=1024,
             activation="relu",
-        )(self.base_model.output)
+        )(self.densenet_basemodel_output)
 
-        self.densenet_custom_dropout_0 = Dropout(
-            name="densenet_custom_dropout_0",
-            rate=0.7,
-        )(self.densenet_custom_dense_0)
-
-        self.densenet_custom_dense_1 = Dense(
-            name="densenet_custom_dense_1",
-            units=128,
-            activation="relu",
-        )(self.densenet_custom_dropout_0)
-
-        self.densenet_custom_dropout_1 = Dropout(
-            name="densenet_custom_dropout_1",
+        self.densenet_custom_layer_1 = Dropout(
+            name="densenet_custom_layer_1",
             rate=0.5,
-        )(self.densenet_custom_dense_1)
+        )(self.densenet_custom_layer_0)
 
-        self.densenet_custom_dense_2 = Dense(
-            name="densenet_custom_dense_2",
-            units=64,
+        self.densenet_custom_layer_2 = Dense(
+            name="densenet_custom_layer_2",
+            units=512,
             activation="relu",
-        )(self.densenet_custom_dropout_1)
-
-        self.densenet_custom_dropout_2 = Dropout(
-            name="densenet_custom_dropout_2",
-            rate=0.3,
-        )(self.densenet_custom_dense_2)
+        )(self.densenet_custom_layer_1)
 
         self.densenet_custom_output = Dense(
             name="densenet_custom_output",
             units=7,
             activation="softmax",
-        )(self.densenet_custom_dropout_2)
+        )(self.densenet_custom_layer_2)
 
-        self.model = Model(inputs=self.base_model.input, outputs=self.densenet_custom_output)
+        self.model = Model(inputs=self.densenet_inputs, outputs=self.densenet_custom_output)
 
-    def compile(self):
+    def compile(self, adam_learning_rate=1e-3):
         self.model.compile(
-            loss=categorical_crossentropy, optimizer=Adam(learning_rate=ADAM_LEARN_RATE)
+            loss=categorical_crossentropy,
+            optimizer=Adam(learning_rate=adam_learning_rate),
+            metrics=["accuracy"]
         )
     
     def fit(
@@ -108,16 +131,25 @@ class DenseNet(object):
     @property
     def layers(self):
         return self.model.layers
+    
+    @property
+    def trainable(self):
+        return self.model.trainable
 
     @property
+    def basemodel(self):
+        return self.base_model
+    
+    @property
     def pre_softmax_layer(self):
-        return self.densenet_custom_dropout_2
+        return self.densenet_custom_layer_2
     
     @property
     def pre_softmax_layer_output(self):
         return function(
-            inputs=[self.input], outputs=[self.densenet_custom_dropout_2]
+            inputs=[self.input], outputs=[self.densenet_custom_layer_2]
         )
+
 
     def get_pre_softmax_layer_output(self, input_batch):
         return self.pre_softmax_layer_output([input_batch])[0]
@@ -129,3 +161,10 @@ class DenseNet(object):
             input_batch = preprocess_input(input_batch)
 
         return self.model.predict(input_batch)
+
+    def evaluate(self, input_batch: Stream):
+
+        if isinstance(input_batch, ndarray):
+            input_batch = preprocess_input(input_batch)
+
+        return self.model.evaluate(input_batch)
